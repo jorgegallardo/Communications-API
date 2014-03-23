4c5489e123e4fefc36685f25aaf002bec692692a
Jorge Gallardo <jorge8486@gmail.com>
2014-03-23 03:13:46 -0400

    using JSONP to retrieve data from another domain

--- mightygumball.js
+++ mightygumball.js
@@ -1,19 +1,20 @@
 window.onload = function() {
-	var url = "http://localhost:8000/sales.json";
-	var request = new XMLHttpRequest(); //constructor initializes XMLHttpRequest
-	request.open("GET", url); //initializes the request, using GET method, and the url provided
+	// var url = "http://gumball.wickedlysmart.com";
+	// var request = new XMLHttpRequest(); //constructor initializes XMLHttpRequest
+	// request.open("GET", url); //initializes the request, using GET method, and the url provided
 
-	request.onload = function() { //handles the response from the server
-		if(request.status == 200) //status property of XMLHttpRequest object, 200 = OK!
-			updateSales(request.responseText); //place JSON response in responseText property of XMLHttpRequest
-	};
+	// request.onload = function() { //handles the response from the server
+	// 	if(request.status == 200) //status property of XMLHttpRequest object, 200 = OK!
+	// 		updateSales(request.responseText); //place JSON response in responseText property of XMLHttpRequest
+	// };
 
-	request.send(null); //method that sends request to server (we pass null because we're not sending data to the service)
+	// request.send(null); //method that sends request to server (we pass null because we're not sending data to the service)
 }
 
-function updateSales(responseText) {
+// function updateSales(responseText) {
+function updateSales(sales) {
 	var salesDiv = document.getElementById("sales");
-	var sales = JSON.parse(responseText); //take response and use JSON.parse to convert it to JavaScript object, assigning it to var sales
+	// var sales = JSON.parse(responseText); //take response and use JSON.parse to convert it to JavaScript object, assigning it to var sales
 
 	for(var i = 0; i < sales.length; i++) {
 		var sale = sales[i];
